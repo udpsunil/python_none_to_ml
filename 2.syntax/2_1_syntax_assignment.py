e7ec37434c67843a10b789e260c4228f18261a34
Sunil Udupi <udpsunil@gmail.com>
2022-12-12 11:04:49 +0530

    Added folder structure and moved the relevant files

--- 2.syntax/2_1_syntax_assignment.py
+++ 2.syntax/2_1_syntax_assignment.py
@@ -6,7 +6,7 @@
 - 'AttributeError' when a variable can not be assigned
 """
 
-## Variables
+# Variables
 
 # identifier are case sensitive
 name = 'Sunil Udupi'
@@ -29,7 +29,7 @@ name1 = 'Sunil'
 # 1name
 
 
-## Constants
+# Constants
 # By convention variable names with all capitals are considered as constants
 FILE = '/etc/passwd'
 FILENAME = '/etc/group'
@@ -39,3 +39,34 @@ FILE_NAME = '/etc/shadow'
 name = 'Sunil Udupi'    # variable
 NAME = 'SUNIL UDUPI'    # constant
 Name = 'Sunil Udupi'    # class
+
+# Examples of Constants by convention
+SECOND = 1
+MINUTE = 60 * SECOND
+HOUR = 60 * MINUTE
+
+workday = 8 * HOUR
+workweek = 40 * HOUR
+
+# Example where Constants convention doesn't make sense.
+Pa = 1
+hPa = 100 * Pa
+kPa = 1000 * Pa
+MPa = 1000000 * Pa
+
+# Declaring above constants in all caps won't help readability
+
+
+## Types
+# Basic Types
+data = 1        # int
+data = 1.2      # float
+data = True     # bool
+data = None     # NoneType
+data = 'abc'    # str
+
+# Sequences
+data = [1, 2, 3,]           # list
+data = (1, 2, 3)            # tuple
+data = {1, 2, 3}            # set
+data = {'a':1, 'b':2, "c":3}# dict
